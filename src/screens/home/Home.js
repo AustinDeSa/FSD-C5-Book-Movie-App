7d2c62527da803d265b39e9d388ba2a4137e5aa2
Austin DeSa <austin.desa.15893@gmail.com>
2022-05-22 15:01:29 +0530

    Fist time commit after adding the required JS and CSS file for starting and running the project

---
+++ src/screens/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../../common/header/Header";
+import "./Home.css";
+
+const Home = function () {
+    return (
+        <div>
+            
+        </div >
+    )
+}
+
+export default Home;
