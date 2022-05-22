20e579f6f8646852a5fae16511ede53777f8153b
Austin DeSa <austin.desa.15893@gmail.com>
2022-05-22 15:49:48 +0530

    Added Home page changes done in Home.js and Home.css file. Also changes done in Controller.js

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -9,13 +9,125 @@ const Controller = () => {
 
 	const baseUrl = "/api/v1/";
 
+	const [genreList, setGenreList] = useState([]);
+	const [artistList, setArtistList] = useState([]);
+	const [moviesList, setMoviesList] = useState([]);
+	const [moviesFilterdList, setMoviesFilteredList] = useState([]);
+
+	// Fetch data for dropdown Genre //
+	async function loadGenre() {
+
+		try {
+			const response = await fetch('http://localhost:8085/api/v1/genres', {
+				headers: {
+					"Accept": "application/json;charset=UTF-8"
+				},
+			});
+			const result = await response.json();
+
+			let genre = [result.genres];
+
+			setGenreList(genre[0]);
+
+		} catch (e) {
+
+			alert(`Error: ${e.message}`);
+
+		}
+	}
+	// Fetch data for dropdown Genre //
+
+	// Fetch data for dropdown Artist //
+	async function loadArtist() {
+
+		try {
+			const response = await fetch('http://localhost:8085/api/v1/artists', {
+				headers: {
+					"Accept": "application/json;charset=UTF-8"
+				},
+			});
+			const result = await response.json();
+
+			let artist = [result.artists];
+
+			setArtistList(artist[0]);
+
+		} catch (e) {
+
+			alert(`Error: ${e.message}`);
+
+		}
+	}
+	// Fetch data for dropdown Artist //
+
+	// Fetch data for Movies List //
+	async function loadMovies() {
+
+		try {
+			const response = await fetch('http://localhost:8085/api/v1/movies?status=PUBLISHED', {
+				headers: {
+					"Accept": "application/json;charset=UTF-8"
+				},
+			});
+			const result = await response.json();
+
+			let movie = [result.movies];
+
+			setMoviesList(movie[0]);
+
+		} catch (e) {
+
+			alert(`Error: ${e.message}`);
+
+		}
+	}
+	// Fetch data for Movies List //
+
+	// Fetch data for Filtered Movies List //
+	async function loadFilteredMovies(moviename, gen, art, startdate, enddate) {
+
+		try {
+			const response = await fetch('http://localhost:8085/api/v1/movies?status=RELEASED&title=' + moviename + '&genre=' + gen + '&artists=' +
+				art + '&start_date=' + startdate + '&end_date=' + enddate, {
+				headers: {
+					"Accept": "application/json;charset=UTF-8"
+				},
+			});
+			const result = await response.json();
+
+			let movie = [result.movies];
+
+			setMoviesFilteredList(movie[0]);
+
+		} catch (e) {
+
+			alert(`Error: ${e.message}`);
+
+		}
+	}
+	// Fetch data for Filtered Movies List //
+
+	// Movies Filter handler
+	function moviesFilterHandler(moviename, gen, art, startdate, enddate) {
+		loadFilteredMovies(moviename, gen, art, startdate, enddate);
+	}
+
+	useEffect(() => {
+		loadArtist();
+		loadGenre();
+		loadMovies();
+		loadFilteredMovies('', '', '', '', '');
+	}, []);
+
 	return (
 		<Router>
 			<div className="main-container">
 				<Route
 					exact
 					path="/"
-					render={(props) => <Home {...props} baseUrl={baseUrl} />}
+					render={(props) => <Home {...props} baseUrl={baseUrl} genreList={genreList} artistList={artistList} moviesList={moviesList}
+						moviesFilterdList={moviesFilterdList}
+						moviesFilterHandler={(moviename, gen, art, startdate, enddate) => moviesFilterHandler(moviename, gen, art, startdate, enddate)} />}
 				/>
 				<Route
 					path="/movie/:id"
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,203 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "../../common/header/Header";
 import "./Home.css";
+import { GridList, GridListTile, GridListTileBar, Card, Button, FormControl, InputLabel, Input, Checkbox, TextField, MenuItem } from '@material-ui/core';
+import Moment from 'moment';
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Home = function () {
+// Style the Grids
+const mainGrid = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+};
+
+const gridList = {
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)',
+};
+
+const gridList2 = {
+    transform: 'translateZ(0)',
+};
+
+const useStyles = makeStyles(theme => ({
+    cardtitle: {
+        color: theme.palette.primary.light,
+        margin: theme.spacing(1),
+    },
+    maincard: {
+        margin: theme.spacing(1),
+    },
+}));
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+const Home = function ({ genreList, artistList, moviesList, moviesFilterdList, moviesFilterHandler }) {
+    // Artist Dropdown //
+    var resart = '';
+    const [art, setArtist] = useState("");
+
+    const handleArtistChange = (e, value) => {
+        for (var i = 0; i < value.length; i++) {
+            if (value.length === 1) {
+                resart = value[i].first_name + ' ' + value[i].last_name;
+            } else {
+                resart = resart + ',' + value[i].first_name + ' ' + value[i].last_name;
+            }
+        }
+        setArtist(resart);
+    };
+    // Artist Dropdown //
+
+    // Genre Dropdown //
+    var resgen = '';
+    const [gen, setGenres] = useState("");
+
+    const handleGenresChange = (e, value) => {
+        for (var i = 0; i < value.length; i++) {
+            if (value.length === 1) {
+                resgen = value[i].genre;
+            } else {
+                resgen = resgen + ',' + value[i].genre;
+            }
+        }
+        setGenres(resgen);
+    }
+    // Genre Dropdown //
+
+    // State for Search Filter //
+    const [addSearchForm, setSearchForm] = useState({
+        id: 0,
+        moviename: '',
+        startdate: '',
+        enddate: '',
+    });
+
+    // Search Filter form onChange 
+    const inputFilterChangedHandler = (e) => {
+        const state = addSearchForm;
+        state[e.target.name] = e.target.value;
+        setSearchForm({ ...state })
+    }
+
+    const { moviename, startdate, enddate } = addSearchForm;
+
+    const onSearchFormSubmitted = (e) => {
+        e.preventDefault();
+
+        moviesFilterHandler(moviename, gen, art, startdate, enddate);
+    }
+    // State for Search Filter //
+
+    // Date format //
+    Moment.locale('en');
+    // Date format //
+
+    const classes = useStyles();
+
     return (
         <div>
-            
+            <Header showButton="btn-hide" />
+            <div className="home-header">Upcoming Movies</div>
+            <div style={mainGrid}>
+                <GridList style={gridList} cols={6} cellHeight={250}>
+                    {moviesList.map(movies => (
+                        <GridListTile key={movies.id}>
+                            <img src={movies.poster_url} alt={movies.title} />
+                            <GridListTileBar
+                                title={movies.title}
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
+            <div className='home-container'>
+                <div className='home-search-movies'>
+                    <div style={mainGrid}>
+                        <GridList style={gridList2} cols={4} cellHeight={350}>
+                            {moviesFilterdList.map(filmovies => (
+                                <GridListTile key={filmovies.id} className="home-grid-tile" component={Link} to={`/movie/${filmovies.id}`}>
+                                    <img src={filmovies.poster_url} alt={filmovies.title} />
+                                    <GridListTileBar
+                                        title={filmovies.title}
+                                        subtitle={"Release Date " + Moment(filmovies.release_date).format('ddd MMM DD yyyy')}
+                                    />
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                </div>
+                <div className='home-search-filter'>
+                    <Card>
+                        <form className="search-form" onSubmit={onSearchFormSubmitted}>
+                            <div className={classes.maincard}>
+                                <div className={classes.cardtitle}>
+                                    FIND MOVIES BY:
+                                </div>
+                                <div className={classes.maincard}>
+                                    <FormControl>
+                                        <InputLabel htmlFor="my-input-name">Movie Name</InputLabel>
+                                        <Input id="my-input-name" aria-describedby="my-helper-text" name="moviename" type='text'
+                                            onChange={inputFilterChangedHandler} className="input-search" value={moviename} />
+                                    </FormControl><br /><br />
+                                    <FormControl>
+                                        <Autocomplete className="input-search" onChange={handleGenresChange}
+                                            name="gen" multiple id="my-input-genre" size="small"
+                                            options={genreList} getOptionLabel={option => option.genre}
+                                            disableCloseOnSelect
+                                            renderOption={(option, { selected }) => (
+                                                <React.Fragment>
+                                                    <Checkbox icon={icon} checkedIcon={checkedIcon} style={{ marginRight: 8 }} checked={selected} />
+                                                    <MenuItem>{option.genre}</MenuItem>
+                                                </React.Fragment>
+                                            )}
+                                            renderInput={params => (
+                                                <TextField {...params} variant="standard" label="Genres" />
+                                            )}
+                                        />
+                                    </FormControl><br /><br />
+                                    <FormControl>
+                                        <Autocomplete className="input-search" onChange={handleArtistChange}
+                                            name="art" multiple id="my-input-art" size="small"
+                                            options={artistList} getOptionLabel={option => option.first_name + ' ' + option.last_name}
+                                            disableCloseOnSelect
+                                            renderOption={(option, { selected }) => (
+                                                <React.Fragment>
+                                                    <Checkbox icon={icon} checkedIcon={checkedIcon} style={{ marginRight: 8 }} checked={selected} />
+                                                    <MenuItem>{option.first_name + ' ' + option.last_name}</MenuItem>
+                                                </React.Fragment>
+                                            )}
+                                            renderInput={params => (
+                                                <TextField {...params} variant="standard" label="Artists" />
+                                            )}
+                                        />
+                                    </FormControl><br /><br />
+                                    <FormControl>
+                                        <InputLabel htmlFor="my-input-startdate" shrink={true}>Release Date Start</InputLabel>
+                                        <TextField id="my-input-startdate" aria-describedby="my-helper-text" name="startdate" type='date'
+                                            onChange={inputFilterChangedHandler} className="input-search" value={startdate} />
+                                    </FormControl><br /><br />
+                                    <FormControl>
+                                        <InputLabel htmlFor="my-input-enddate" shrink={true}>Release Date End</InputLabel>
+                                        <TextField id="my-input-enddate" aria-describedby="my-helper-text" name="enddate" type='date'
+                                            onChange={inputFilterChangedHandler} className="input-search" value={enddate} />
+                                    </FormControl><br /><br />
+                                </div>
+                                <div className={classes.maincard}>
+                                    <Button type="submit" variant="contained" color="primary" className='input-search search-btn'>APPLY</Button>
+                                </div>
+                            </div>
+                        </form>
+                    </Card>
+                </div>
+            </div>
         </div >
     )
 }
